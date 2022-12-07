ba9c038530a084abf07f72c6729ee0ba6b0cd4f8
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-12-07 18:16:26 +0000

    working on hero section

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,18 +1,22 @@
 <script>
     import rings from '$lib/images/pattern-rings.svg'
+    import profile from '$lib/images/image-profile-desktop.webp'
 
 </script>
 
 
 <div class="hero-wrapper">
     <section class="container hero">
-        <h1>Nice To Meet You! <span class="break">I'm Chris Bernhard.</span></h1>
-        <img src="{rings}" alt="">
+        <img src="{rings}" alt="Rings that make a pattern for the background" class="background">
+        <img src="{profile}" alt="Profile of Christopher Bernhard" class="profile-pic">
+        <h1>Nice To Meet You! <span class="block">I'm <span class="underline">Chris Bernhard</span>.</span></h1>
+        <p>
+            Based in Chicago, IL, I'm a front-end developer passionate about building accessible web apps that users love.
+        </p>
+        <a href="/" class="btn">Contact Me</a>
+        
     </section>
 </div>
-
-
-
 
 <style>
     .hero-wrapper {
@@ -22,10 +26,35 @@
     .hero {
         height: 100%;
         display: flex;
-        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        position: relative;
+        gap: 5rem;
     }
 
-    .break {
-        display: block;
+    h1 {
+        font-size: 7rem;
+        font-weight: 400;
+    }
+
+    p {
+        font-size: 1.6rem;
+        max-width: 40ch;
+    }
+
+    .background {
+        position: absolute;
+        max-width: 50rem;
+        top: 100px;
+        left: -200px;
+    }
+
+    .profile-pic {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 500px;
+        z-index: -999;
     }
 </style>
